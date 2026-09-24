87b9cdc40167768550a6be1bf7a247e852d3eca1
iamsyifaa <syifaoktaviani1710@gmail.com>
2026-09-24 11:57:23 +0700

    menambahkan home&profil

--- resources/views/class/index.blade.php
+++ resources/views/class/index.blade.php
@@ -8,11 +8,12 @@
         <p class="fs-5 text-muted">Ini adalah website landing page untuk menampilkan profil kelompok, daftar anggota kelompok
             dan kontak kelompok.</p>
             <hr>
-        <P>Nama Kelas: XI RPL 1</P>
-        <P>Nama Sekolah: SMKN 1 Garut </P>
-        <P>Program Keahlian: Rekayasa Perangkat Lunak</P>
-        <P>Deskripsi Kelas: Kelas 11 RPL 1 </P>
-        <P>Visi Kelas: Menjadi kelas yang unggul dalam bidang Rekayasa Perangkat Lunak.</P>
+        <p>Kelompok 5</p>
+        <p>Nama Kelas: XI RPL 1</p>
+        <p>Nama Sekolah: SMKN 1 Garut </p>
+        <p>Program Keahlian: Rekayasa Perangkat Lunak</p>
+        <p>Deskripsi Kelas: Kelas 11 RPL 1 </p>
+        <p>Visi Kelas: Menjadi kelas yang unggul dalam bidang Rekayasa Perangkat Lunak.</p>
         <a href="{{ route('class.kontak') }}" class="btn btn-primary">Lihat Kontak</a>
     </div>
 @endsection
